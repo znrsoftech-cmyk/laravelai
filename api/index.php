4ed506bc83d268c86e80e2cf668b1eba659b37c6
zaheer8abbas <zaheer8abbas@gmail.com>
2026-09-11 18:30:56 +0530

    Force add api/index.php entrypoint

--- api/index.php
+++ api/index.php
@@ -1,17 +1,4 @@
 <?php
 
-use Illuminate\Foundation\Application;
-use Illuminate\Http\Request;
-
-define('LARAVEL_START', microtime(true));
-
-if (file_exists($maintenance = __DIR__.'/../storage/framework/maintenance.php')) {
-    require $maintenance;
-}
-
-require __DIR__.'/../vendor/autoload.php';
-
-/** @var Application $app */
-$app = require_once __DIR__.'/../bootstrap/app.php';
-
-$app->handleRequest(Request::capture());
+// Forward Vercel requests to the normal Laravel index file
+require __DIR__ . '/../public/index.php';
